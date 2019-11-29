2bb8988a103285aa64fa2f0bdbf159dd226a21ad
Ronny Maurhofer <ronny.maurhofer@hotmail.com>
2019-11-29 17:54:42 +0100

    prediction array v1 added

--- arosa_app/api/predictions.py
+++ arosa_app/api/predictions.py
@@ -1,5 +1,53 @@
+import pandas as pd
+import pprint
+from calendar import monthrange
+
+
 def get_predictions(year, month):
-    return [
-        {'date': '2019-08-01', 'predictions': {'counter': 10, 'mail': 2, 'tel': 5}},
-        {'date': '2019-08-02', 'predictions': {'counter': 5, 'mail': 8, 'tel': 7}}
-    ]
+    """
+    month = Month to be predicted, expected format MM
+    year = Year of month to be predicted, expected format yyyy
+    """
+    
+    start_date = str(year)+"-"+str(month)+"-"+str(monthrange(year, month)[0])
+    end_date = str(year)+"-"+str(month)+"-"+str(monthrange(year, month)[1])
+
+    date_range = pd.date_range(start_date,end_date, freq='D').strftime("%Y-%m-%d").tolist()
+
+    print(date_range)
+
+    # predictfunction
+    # ..
+    
+    pred_arr = []
+    
+    
+    for date in date_range:
+        
+        pred_mail = 2
+        pred_tel = 5
+        pred_counter = 10
+        
+        pred_dict = {'date': date,  
+                     'predictions':{'mail' : pred_mail, 
+                                    'tel' : pred_tel, 
+                                    'counter' : pred_counter
+                                    }
+                     }
+        
+
+    
+        pred_arr.append(pred_dict)
+    
+
+        
+    pprint.pprint(pred_arr)
+    #pprint.pprint(date_range)
+    
+    return pred_arr
+
+
+
+#if __name__ == "__main__":
+#    get_predictions(8,2019)
+
