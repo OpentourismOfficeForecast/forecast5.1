a947de77c683bbf48c71ff805a3e6a2cc3559251
Ronny Maurhofer <ronny.maurhofer@hotmail.com>
2019-11-30 09:57:50 +0100

    read predictions

--- arosa_app/api/predictions.py
+++ arosa_app/api/predictions.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from calendar import monthrange
-import numpy as np
 
 
 def get_predictions(year, month):
@@ -14,34 +13,65 @@ def get_predictions(year, month):
 
     date_range = pd.date_range(start_date,end_date, freq='D').strftime("%Y-%m-%d").tolist()
 
-    # predictfunction
-    # ..
-    
+    # predictfunction    
     pred_arr = []
+    #predictions = pd.DataFrame(data = date_range,columns=['Datum'])
+    predictions = load_predictions('../predictions/rf1.csv')
+    predictions = predictions.round(1)
     
+    #pred_mail = np.random.randint(low=1, high=100)
+    #pred_counter = np.random.randint(low=1, high=100)
+    #pred_tel = np.random.randint(low=1, high=100)
     
-    for date in date_range:
+    for index,row in predictions.iterrows():
         
-        pred_mail = np.random.randint(low=1, high=100)
-        pred_tel = np.random.randint(low=1, high=100)
-        pred_counter = np.random.randint(low=1, high=100)
+        # check predictions dataframe for 'Datum'
+        if 'Datum' in predictions.columns:
+            date = row['Datum']
+        else:
+            break;
+
+        # check predictions dataframe for 'Mail'
+        if 'Mail' in predictions.columns:
+            pred_mail = row['Mail']
+        else:
+            pred_mail = 0
+
+        # check predictions dataframe for 'Schalter'
+        if 'Schalter' in predictions.columns:
+            pred_counter = row['Schalter']
+        else:
+            pred_counter = 0
+
+        # check predictions dataframe for 'Tel'
+        if 'Tel' in predictions.columns:
+            pred_tel = row['Tel']
+        else:
+            pred_counter = 0
+    
         
         pred_dict = {'date': date,  
                      'predictions':{'mail' : pred_mail, 
                                     'tel' : pred_tel, 
                                     'counter' : pred_counter
-                                    }
-                     }
-        
-   
+                                   }
+                    }
+
         pred_arr.append(pred_dict)
 
-    print(pred_arr)
+    print(pred_arr)    
     
     return pred_arr
 
 
+def load_predictions(file, delimiter=';'):
+    pred_df = pd.read_csv(file, delimiter=delimiter)
+    
+    return pred_df
+
+
 
 #if __name__ == "__main__":
-#    get_predictions(2019,10)
+    #get_predictions(2019,10)
+    #pred = load_predictions('../predictions/rf1.csv')
 
